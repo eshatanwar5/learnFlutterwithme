2cc9cedb063ba7f65743a2ab987ea1b2417d8116
eshatanwar5 <91387753+eshatanwar5@users.noreply.github.com>
2022-07-10 12:35:33 +0530

    Day 4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:meeapp/pages/login_page.dart';
 
 import 'pages/home_page.dart';
@@ -13,13 +14,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // home: Homepage(),
       themeMode: ThemeMode.light, //light mode
-      theme: ThemeData(primarySwatch: Colors.yellow),
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        primaryTextTheme: GoogleFonts.latoTextTheme()//ek jagah change krdia taki puri   jagah change hojaye
+
+          ),
+
       darkTheme: ThemeData(
         //dark mode
         primarySwatch: Colors.red,
         brightness: Brightness.dark,
       ), //to make ui good
-        initialRoute:"/home",
+        initialRoute:"/",
       routes: {
 
         "/": (context) => LoginPage(), //by default login page pe ayegaa app m
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,20 +5,67 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material( 
-      child: Center(
-        child : Text(
-          "Login Page",
-          style:TextStyle(
-            fontSize: 40,
-            color: Colors.blue,
-            fontWeight: FontWeight.bold,
-          ),
-          ),
-       
+    return Material(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.cover,
+            ), //size dena h fit m
+            SizedBox(
+              height: 20.0,
+            ),
 
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                  vertical: 16.0,
+                  horizontal: 32.0), //column liya or wrap krdiya padding s
+              child: Column(
+                //symetric isliy taki vertical or  horizontal change laasake
+                children: [
+                  TextFormField(
+                    //obscure.Text
+               //password ko show nahi karta h  bydefault false hota h true krdege toh nhi dikega password
+                    decoration: InputDecoration(
+                      hintText: "Enter UserName",
+                      labelText: "Username",
+                    ),
+                  ),
+                  TextFormField(
+                  obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "password",
+                    ),
+                  ),
+                  SizedBox(
+                    //for submit  button
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                    child: Text("Login"),
+                    style: TextButton
+                        .styleFrom(), //light krdega  submit button ka text
 
-    ),
-    );
+                    onPressed: () {
+                      print("hi esha");
+                    },
+                  )
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
